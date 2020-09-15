4e0971f48816602386e4e51cdf22b16527be8c5c
Alanna Bouloy <lanihope@gmail.com>
2020-09-15 15:30:00 -0400

    addItem function works

--- script.js
+++ script.js
@@ -15,6 +15,17 @@ function main(){
 }
 //function to add list-item to DOM
 function addItem(listItem){
+  $('.shopping-list').append(` <li>
+    <span class="shopping-item">${listItem}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
   console.log(`${listItem} added`);
 }
 //function to check item off list
