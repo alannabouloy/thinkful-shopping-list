d821699594b9dc66734c97e480e2bee7a924764a
Alanna Bouloy <lanihope@gmail.com>
2020-09-15 16:17:49 -0400

    minor change

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function main(){
     event.preventDefault(); 
     let listItem = $('#shopping-list-entry').val();
     addItem(listItem);
-  
+    console.log('main is working');
+    
   });
   checkItem();
   deleteItem();
@@ -26,13 +27,15 @@ function addItem(listItem){
       </button>
     </div>
   </li>`);
+  console.log(`${listItem} added`);
 }
 
 //function to check item off list
 function checkItem(){
   $('.shopping-list').on('click','.shopping-item-toggle', event => {
     $(event.currentTarget).parent().siblings('.shopping-item').toggleClass('shopping-item__checked');
-  });
+  } );
+  console.log('checkItem is working');
 }
 
 //function to delete item
@@ -40,6 +43,7 @@ function deleteItem(){
   $('.shopping-list').on('click', '.shopping-item-delete', event => {
     $(event.currentTarget).parentsUntil('ul').remove();
   });
+  console.log('deleteItem is working');
 }
 
 $(main);
